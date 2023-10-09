8cfb6b3bc5ebf8325276b1db7b1de9dfe3a69577
A Krishnaprasad <krishna.arakkal@gmail.com>
2023-10-09 12:41:37 +0530

    idk man...forgot

--- com.acabes.assignments.java/src/test/java/com/acabes/assignments/java/fileprograms/OneLakhNames.java
+++ com.acabes.assignments.java/src/test/java/com/acabes/assignments/java/fileprograms/OneLakhNames.java
@@ -27,7 +27,8 @@ public class OneLakhNames {
             for(i = 0; i<1000000; i++){
                 fileOutputStream.write(i);
             }
-            System.out.println("Writing by FileOutputStream: " + (System.currentTimeMillis() - start) + "ms");
+            float fileOutputStreamTime = System.currentTimeMillis() - start;
+            System.out.println("Writing by FileOutputStream: " + fileOutputStreamTime + "ms");
             fileOutputStream.close();
 
             /*FileWriter*/
@@ -36,8 +37,8 @@ public class OneLakhNames {
             for(i=0; i<1000000; i++){
                 fileWriter.write(i + " ");
             }
-            System.out.println("Writing by FileWriter: " + (System.currentTimeMillis() - start) + "ms");
-            fileWriter.close();
+            float FileWriterTime = System.currentTimeMillis() - start;
+            System.out.println("Writing by FileWriter: " + FileWriterTime + "ms");
 
             /*BufferedWriter*/
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
@@ -45,15 +46,22 @@ public class OneLakhNames {
             for(i=0; i<1000000; i++){
                 bufferedWriter.write((int) i);
             }
-            System.out.println("Writing by BufferedWriter: " + (System.currentTimeMillis() - start) + "ms");
+            float BufferedWriterTime = System.currentTimeMillis() - start;
+            System.out.println("Writing by BufferedWriter: " + BufferedWriterTime + "ms");
             bufferedWriter.close();
+            fileWriter.close();
+
+
+            System.out.println("\nBuffered Writer is " + ((1-(BufferedWriterTime/FileWriterTime))*100) + "% faster than FileWriter" +
+                                "\n and " + ((1-(BufferedWriterTime/fileOutputStreamTime))*100) + "% faster than FileOutputStream");
 
             /*FileInputStream*/
             FileInputStream fileInput = new FileInputStream(file);
             BufferedInputStream bufferedInput = new BufferedInputStream(fileInput);
             start = System.currentTimeMillis();
             while((bufferedInput.read()) != -1){}
-            System.out.println("\n\nReading by BufferedInputStream: " + (System.currentTimeMillis() - start) + "ms");
+            int bufferedInputTime = (int) (System.currentTimeMillis() - start);
+            System.out.println("\n\nReading by BufferedInputStream: " + bufferedInputTime + "ms");
             bufferedInput.close();
             fileInput.close();
 
@@ -62,7 +70,8 @@ public class OneLakhNames {
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             start = System.currentTimeMillis();
             while((bufferedReader.readLine()) != null){}
-            System.out.println("Reading by BufferedReader: " + (System.currentTimeMillis() - start) + "ms");
+            int bufferedReaderTime = (int) (System.currentTimeMillis() - start);
+            System.out.println("Reading by BufferedReader: " + bufferedReaderTime + "ms");
             bufferedReader.close();
             fileReader.close();
 
@@ -72,8 +81,13 @@ public class OneLakhNames {
             while(fileScanner.hasNextLine()){
                 String s = fileScanner.nextLine();
             }
-            System.out.println("Reading by Scanner: " + (System.currentTimeMillis() - start) + "ms");
+            int scannerTime = (int) (System.currentTimeMillis() - start);
+            System.out.println("Reading by Scanner: " + scannerTime + "ms");
             fileScanner.close();
+
+            /*System.out.println("\nBufferedReader is " + ((1-(bufferedReaderTime/bufferedInputTime))*100) + "% faster than BufferedInputStream" +
+                    "\n and " + ((1-(bufferedReaderTime/scannerTime))*100) + "% faster than Scanner Class");*/
+
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
